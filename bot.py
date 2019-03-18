bdfdf988bf003106d03cc8440bec286130116cf5
BuildTools <unconfigured@null.spigotmc.org>
2019-03-18 15:14:09 +0100

    First launch

---
+++ bot.py
@@ -0,0 +1,254 @@
+from discord.ext import commands
+import discord, chalk, asyncio, random, time, datetime
+from datetime import datetime
+
+bot = commands.Bot(command_prefix="-", status=discord.Status.idle, activity=discord.Game(name="Starting up..."))
+client = discord.Client()
+
+bot.remove_command("help")
+
+
+@bot.command()
+async def setstatus(ctx, type, *, name):
+    user = bot.get_user(445556389532925952)
+    user2 = bot.get_user(394174323117654036)
+    if ctx.message.author == user or ctx.message.author == user2:
+
+        if type == "listening":
+            await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f"{name}"))
+            await ctx.channel.send(f"Set the status to **Listening to {name}**")
+        elif type == "watching":
+            await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{name}"))
+            await ctx.channel.send(f"Set the status to **Watching {name}**")
+        elif type == "playing":
+            await bot.change_presence(activity=discord.Game(name=f"{name}"))
+            await ctx.channel.send(f"Set the status to **playing {name}**")
+        else:
+            await ctx.channel.send("Not a valid type of status")
+    else:
+        await ctx.channel.send("Oops, you cant to that")
+
+@bot.event
+async def on_ready():
+    print(chalk.green("Ready to go!"))
+    print(chalk.blue(f"Serving: {len(bot.guilds)} guilds."))
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game(name="Tick Tick"))
+
+@bot.command()
+@commands.has_any_role("Support")
+async def setup(ctx):
+    guild = ctx.message.guild
+    embed = discord.Embed(title="Setting things up", color=discord.Colour(0xFF0000))
+    msg = await ctx.channel.send(embed=embed)
+    await guild.create_category_channel("Tickets")
+    await guild.create_category_channel("Applications")
+    embed = discord.Embed(title="Done", color=discord.Colour(0x00ff00), description="Do -help to get started")
+    embed.set_footer(text="We have finished setting things up for you")
+
+    await msg.edit(embed=embed)
+
+
+
+
+
+
+
+@bot.command()
+async def botver(ctx):
+    await ctx.channel.send(f"{discord.__version__}")
+
+bot.launch_time = datetime.utcnow()
+
+@bot.command()
+async def uptime(ctx):
+    delta_uptime = datetime.utcnow() - bot.launch_time
+    hours, remainder = divmod(int(delta_uptime.total_seconds()), 3600)
+    minutes, seconds = divmod(remainder, 60)
+    days, hours = divmod(hours, 24)
+    await ctx.send(f"{days}d, {hours}h, {minutes}m, {seconds}s")
+
+
+
+
+
+
+        
+@bot.command()
+async def ping(ctx):
+    ping_ = bot.latency
+    ping = round(ping_ * 1000)
+    embed = discord.Embed(title=f"The bots ping is {ping}ms :ping_pong: ", colour=discord.Colour(0x9adc86),)
+
+    await ctx.channel.send(embed=embed)
+
+
+
+
+
+@bot.command()
+async def close(ctx, closemember:discord.User = None):
+    if closemember == None:
+        user = bot.get_user(ctx.message.author.id)
+    else:
+        user = bot.get_user(closemember.id)
+    
+    channel = bot.get_channel(ctx.channel.id)
+    if channel.category.name == "Tickets":
+        await channel.delete()
+        embed = discord.Embed(title="Your ticket has been closed", colour=discord.Colour(0xf6ff), description=f"Thank you for using {ctx.message.guild.name}")
+        await user.send(embed=embed)
+    else:
+        await ctx.channel.send("This is not a ticket...")
+
+@bot.command()
+async def withdraw(ctx):
+    
+    channel = bot.get_channel(ctx.channel.id)
+    if channel.category.name == "Applications":
+        await channel.delete()
+        embed = discord.Embed(title="Your application has been closed", colour=discord.Colour(0xf6ff), description=f"Thank you for using {ctx.message.guild.name}")
+        await user.send(embed=embed)
+    else:
+        await ctx.channel.send("This is not a application...")
+
+
+
+
+@bot.command(aliases=["app", "application"])
+async def apply(ctx, *, type = None):
+    if type != None:
+
+        num = random.randint(0,999)
+        if num < 10:
+            num = f"00{num}"
+        if num < 100:
+            num = f"0{num}"
+        guild = ctx.message.guild
+        tick = discord.utils.get(guild.categories, name='Applications')
+
+        channel = await guild.create_text_channel(f"application-{num}", category=tick)
+        await channel.set_permissions(ctx.message.author, read_messages=True, send_messages=True)
+
+        embed = discord.Embed(title="Your application has been created ", colour=discord.Colour(0xf6ff), description=f"We created a channel for your application in {channel.mention}")
+        
+        await ctx.channel.send(embed=embed)
+        embed = discord.Embed(title=f"{ctx.message.author.name}, We will get right back to you ", colour=discord.Colour(0xff00), description="Your application has been created. We will come right back to you! In the mean time please describe what you can do and why your applying.")
+
+        embed.set_thumbnail(url=f"{ctx.message.author.avatar_url}")
+        embed.set_author(name=f"{type}")
+        embed.set_footer(text=f"Thank you for choosing {ctx.message.guild.name}")
+        await channel.send(embed=embed)
+
+    else:
+        embed = discord.Embed(title="Please tell us what you want to apply for", colour=discord.Colour(0xf6ff), description="eg. -apply Configuring plugins")
+
+        await ctx.channel.send(embed=embed)
+
+@bot.command()
+async def add(ctx, person:discord.User):
+    channel = bot.get_channel(ctx.channel.id)
+    if user == None:
+        await ctx.channel.send("Please mention a user to add")
+    else:
+        if channel.category.name == "Tickets":
+            await channel.set_permissions(person, read_messages=True, send_messages=True)
+            await ctx.channel.send(f"Added {person.mention} to this ticket")
+        else:
+            await ctx.channel.send("This is not a ticket...")
+
+@bot.command()
+async def remove(ctx, person:discord.User):
+    channel = bot.get_channel(ctx.channel.id)
+    if user == None:
+        await ctx.channel.send("Please mention a user to remove")
+    else:
+        if channel.category.name == "Tickets":
+            await channel.set_permissions(person, read_messages=False, send_messages=False)
+            await ctx.channel.send(f"Removed {person.name} from this ticket")
+        else:
+            await ctx.channel.send("This is not a ticket...")
+
+@bot.command()
+@commands.has_any_role("Support")
+async def claim(ctx, id:int):
+    channel = bot.get_channel(id)
+    if channel.category.name == "Tickets":
+        await channel.set_permissions(ctx.message.author, read_messages=True, send_messages=True) 
+        await ctx.channel.send(f"You have claimed {channel.mention}")
+        await channel.send(f"{ctx.message.author.mention} is now assigned to your ticket")
+    else:
+        await ctx.channel.send("You cant claim this")
+
+
+@bot.command(aliases=["tickets", "ticket", "make", "order"])
+async def new(ctx, *, type = None):
+    if type != None:
+
+        num = random.randint(0,999)
+        if num < 10:
+            num = f"00{num}"
+        if num < 100:
+            num = f"0{num}"
+        guild = ctx.message.guild
+        tick = discord.utils.get(guild.categories, name='Tickets')
+        channel = await guild.create_text_channel(f"ticket-{num}", category=tick)
+        await channel.set_permissions(ctx.message.author, read_messages=True, send_messages=True)
+
+        embed = discord.Embed(title="Your ticket has been created ", colour=discord.Colour(0xf6ff), description=f"We created a channel for your ticket in {channel.mention}")
+        
+        await ctx.channel.send(embed=embed)
+        embed = discord.Embed(title=f"{ctx.message.author.name}, We will get right back to you ", colour=discord.Colour(0xff00), description="Your ticket has been created. We will come right back to you! In the mean time please describe your questions thoroughly.")
+
+        embed.add_field(name="To get faster support check out #prices", value="We will get back to you faster than other custumers", inline=False)
+
+        embed.set_thumbnail(url=f"{ctx.message.author.avatar_url}")
+        embed.set_author(name=f"{type}")
+        embed.set_footer(text=f"Thank you for choosing {ctx.message.guild.name}")
+        role = discord.utils.get(guild.roles, name='Support')
+
+        await channel.send(content=f"{role.mention}",embed=embed)
+
+
+
+    else:
+        embed = discord.Embed(title="Please tell us what you need", colour=discord.Colour(0xf6ff), description="eg. -new Discord Bot")
+
+        await ctx.channel.send(embed=embed)
+
+
+@bot.command()
+async def user(ctx, member:discord.User = None):
+    if member == None:
+        member = ctx.message.author
+        pronoun = "Your"
+    else:
+        pronoun = "Their"
+    name = f"{member.name}#{member.discriminator}"
+    mention = member.mention
+    status = member.status
+    joined = member.joined_at
+    role = member.top_role
+    await ctx.channel.send(f"{pronoun} name is {name} and thier id is {member.id}. {pronoun} status is {status}. They joined at {joined}. {pronoun} rank is {role}.")
+
+
+@bot.command()
+@commands.has_any_role("Support")
+async def clear(ctx):
+    deleted = await ctx.channel.purge(limit=10000)
+    embed = discord.Embed(title=f"Cleared all of the messages", colour=discord.Colour(0x9adc86))
+    embed.set_footer(text="Message will delete in 10 secs with all other messages sent.")
+    await ctx.channel.send(embed=embed)
+    await asyncio.sleep(10)
+    deleted2 = await ctx.channel.purge(limit=10000)
+
+
+
+@bot.command()
+@commands.has_any_role("Support")
+async def cr(ctx, id:int, *, name):
+    guild = ctx.message.guild
+
+    cat = bot.get_channel(id)
+    channel = await guild.create_text_channel(f"{name}", category=cat)
+
+bot.run("NTU3MTU0OTAzNTYzMzA0OTYw.D3ELQg.687msGFGIfKnJk8ra8AGF0YpxSc")
